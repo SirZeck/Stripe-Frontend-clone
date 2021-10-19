75af987fdde377069e4ce2ad59bdf376bd19549f
Aditya Raj <adithebest.ar@gmail.com>
2021-10-19 19:11:54 +0530

    project added

---
+++ data.js
@@ -0,0 +1,60 @@
+const sublinks = [
+  {
+    page: 'products',
+    links: [
+      {
+        label: 'payment',
+        icon: 'fas fa-credit-card',
+        url: 'https://twitter.com/urfrndlyneighbr',
+      },
+      {
+        label: 'terminal',
+        icon: 'fas fa-credit-card',
+        url: 'https://www.linkedin.com/in/aditraj/',
+      },
+      {
+        label: 'connect',
+        icon: 'fas fa-credit-card',
+        url: 'https://github.com/SirZeck',
+      },
+    ],
+  },
+  {
+    page: 'developers',
+    links: [
+      {
+        label: 'plugins',
+        icon: 'fas fa-book',
+        url: 'https://twitter.com/urfrndlyneighbr',
+      },
+      {
+        label: 'libraries',
+        icon: 'fas fa-book',
+        url: 'https://www.linkedin.com/in/aditraj/',
+      },
+      { label: 'API', icon: 'fas fa-book', url: 'https://github.com/SirZeck' },
+      {
+        label: 'billing',
+        icon: 'fas fa-book',
+        url: 'https://twitter.com/urfrndlyneighbr',
+      },
+    ],
+  },
+  {
+    page: 'company',
+    links: [
+      {
+        label: 'about',
+        icon: 'fas fa-briefcase',
+        url: 'https://twitter.com/urfrndlyneighbr',
+      },
+      {
+        label: 'customers',
+        icon: 'fas fa-briefcase',
+        url: 'https://github.com/SirZeck',
+      },
+    ],
+  },
+]
+
+export default sublinks
